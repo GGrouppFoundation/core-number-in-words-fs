be98991fd3cfc7e78e61329ce76696ede809b13f
pmosk <pm@ggroupp.ru>
2021-09-23 18:18:37 +0300

    Tests refeactoring

---
+++ src/NumberInWords.Tests/TestHelper/TestHelper.fs
@@ -0,0 +1,8 @@
+﻿namespace GGroupp.Core.Tests
+
+open Xunit
+
+module internal TestHelper =
+
+    let internal shouldBeEqual expected actual =
+        Assert.Equal(expected = expected, actual = actual)
